886b811c544270e44b74f8b7d59e494af37808d6
Chad Smith <chadsmith27@gmail.com>
2017-03-01 10:21:49 -0800

    add alias -n argument for no browser; update screenshot; update readme

--- examples/hello.c
+++ examples/hello.c
@@ -1,12 +1,28 @@
 #include<stdio.h>
 
+
+struct mystruct_t{
+	int a;
+	struct{
+		float b;
+		struct{
+			double c;
+		};
+	};
+};
+
 int main(void) {
     printf("Hello World\n");
 
-    for(int i=0; i < 10; i++)
+    for(int i=0; i < 3; i++)
     {
     	printf("i is %d\n", i);
     }
+
+    struct mystruct_t s;
+    s.a = 2;
+    s.b = 1.9;
+    s.c = 899.0;
 
     printf("Goodbye World\n");
     return 0;
