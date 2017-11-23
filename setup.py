c2344a5c40112b70dd71bca92b0bcbb3aa2d9c93
Chad Smith <cs01@users.noreply.github.com>
2017-11-22 19:29:52 -0800

    increment version due to pypi upload error

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ class TestCommand (Command):
 
 setup(
     name='gdbgui',
-    version='0.9.0.0',
+    version='0.9.0.1',
     author='Chad Smith',
     author_email='grassfedcode@gmail.com',
     description=('browser-based gdb frontend using Flask and JavaScript to visually debug C, C++, Go, or Rust'),
