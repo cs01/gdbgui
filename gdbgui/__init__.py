1154e99cc17d65ca01155da563fb331115c516bc
Chad Smith <cs01@users.noreply.github.com>
2018-03-09 14:32:34 -0800

    catch exception and increment version

--- gdbgui/__init__.py
+++ gdbgui/__init__.py
@@ -3,7 +3,10 @@ import os
 import sys
 
 _base_dir = getattr(sys, '_MEIPASS', os.path.dirname(os.path.realpath(__file__)))
-_version = io.open(os.path.join(_base_dir, 'VERSION.txt'), 'r', encoding="utf-8").read().strip()
+try:
+    _version = io.open(os.path.join(_base_dir, 'VERSION.txt'), 'r', encoding="utf-8").read().strip()
+except Exception:
+    _version = '0.11.2.1'
 
 __title__ = 'gdbgui'
 __version__ = _version
