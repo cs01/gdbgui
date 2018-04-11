f1d66c9159bf1291373d9961d020b36f93fe6e35
Chad Smith <cs01@users.noreply.github.com>
2018-04-10 21:09:56 -0700

    add groups to command line arguments; use logging module

--- gdbgui/__init__.py
+++ gdbgui/__init__.py
@@ -3,10 +3,7 @@ import os
 import sys
 
 _base_dir = getattr(sys, '_MEIPASS', os.path.dirname(os.path.realpath(__file__)))
-try:
-    _version = io.open(os.path.join(_base_dir, 'VERSION.txt'), 'r', encoding="utf-8").read().strip()
-except Exception:
-    _version = '0.11.2.1'
+_version = io.open(os.path.join(_base_dir, 'VERSION.txt'), 'r', encoding="utf-8").read().strip()
 
 __title__ = 'gdbgui'
 __version__ = _version
--- gdbgui/statemanager.py
+++ gdbgui/statemanager.py
@@ -1,15 +1,17 @@
-import re
-import platform
-import traceback
-from copy import deepcopy
 from collections import defaultdict
+from copy import deepcopy
 from pygdbmi.gdbcontroller import GdbController
+import logging
+import platform
+import re
+import traceback
 
 
-REQUIRED_GDB_FLAGS = ['--interpreter=mi2']
+REQUIRED_GDB_FLAGS = ["--interpreter=mi2"]
+logger = logging.getLogger(__name__)
 
 startup_with_shell_off = False
-darwin_match = re.match('darwin-(\d+)\..*', platform.platform().lower())
+darwin_match = re.match("darwin-(\d+)\..*", platform.platform().lower())
 if darwin_match is not None and int(darwin_match.groups()[0]) >= 16:
     # if mac OS version is 16 (sierra) or higher, need to set shell off due to
     # os's security requirements
@@ -19,19 +21,14 @@ if darwin_match is not None and int(darwin_match.groups()[0]) >= 16:
 class StateManager(object):
 
     def __init__(self, config):
-        self.controller_to_client_ids = defaultdict(list)  # key is controller, val is list of client ids
+        self.controller_to_client_ids = defaultdict(
+            list
+        )  # key is controller, val is list of client ids
         self.gdb_reader_thread = None
         self.config = config
 
-    def dbprint(self, *args):
-        """print only if app.debug is truthy"""
-        if self.config['DEBUG']:
-            CYELLOW2 = '\33[93m'
-            NORMAL = '\033[0m'
-            print(CYELLOW2 + 'DEBUG: ' + ' '.join(args) + NORMAL)
-
     def connect_client(self, client_id, desired_gdbpid):
-        message = ''
+        message = ""
         pid = 0
         error = False
         using_existing = False
@@ -41,45 +38,59 @@ class StateManager(object):
 
             if controller:
                 self.controller_to_client_ids[controller].append(client_id)
-                message = 'gdbgui is using existing subprocess with pid %s' % str(desired_gdbpid)
+                message = "gdbgui is using existing subprocess with pid %s" % str(
+                    desired_gdbpid
+                )
                 using_existing = True
                 pid = desired_gdbpid
             else:
-                print('error! could not find that pid')
-                message = 'Could not find a gdb subprocess with pid %s. ' % str(desired_gdbpid)
+                print("error! could not find that pid")
+                message = "Could not find a gdb subprocess with pid %s. " % str(
+                    desired_gdbpid
+                )
                 error = True
 
         if self.get_controller_from_client_id(client_id) is None:
-            self.dbprint('new sid', client_id)
+            logger.info("new sid", client_id)
 
-            gdb_args = deepcopy(self.config['initial_binary_and_args']) + REQUIRED_GDB_FLAGS
+            gdb_args = deepcopy(
+                self.config["initial_binary_and_args"]
+            ) + REQUIRED_GDB_FLAGS
 
             if startup_with_shell_off:
                 # macOS Sierra (and later) may have issues with gdb. This should fix it, but there might be other issues
                 # as well. Please create an issue if you encounter one since I do not own a mac.
                 # http://stackoverflow.com/questions/39702871/gdb-kind-of-doesnt-work-on-macos-sierra
-                gdb_args.append('--init-eval-command=set startup-with-shell off')
+                gdb_args.append("--init-eval-command=set startup-with-shell off")
 
-            if self.config['gdb_cmd_file']:
-                gdb_args.append('-x=%s' % self.config['gdb_cmd_file'])
+            if self.config["gdb_cmd_file"]:
+                gdb_args.append("-x=%s" % self.config["gdb_cmd_file"])
 
-            controller = GdbController(gdb_path=self.config['gdb_path'], gdb_args=gdb_args, rr=self.config['rr'])
+            controller = GdbController(
+                gdb_path=self.config["gdb_path"],
+                gdb_args=gdb_args,
+                rr=self.config["rr"],
+            )
             self.controller_to_client_ids[controller].append(client_id)
 
             pid = self.get_pid_from_controller(controller)
-            message += 'gdbgui spawned subprocess with pid %s from executable %s.' % (str(pid), self.config['gdb_path'])
+            message += "gdbgui spawned subprocess with pid %s from executable %s." % (
+                str(pid), self.config["gdb_path"]
+            )
 
-        return {'pid': pid,
-                'message': message,
-                'error': error,
-                'using_existing': using_existing}
+        return {
+            "pid": pid,
+            "message": message,
+            "error": error,
+            "using_existing": using_existing,
+        }
 
     def remove_gdb_controller_by_pid(self, gdbpid):
         controller = self.get_controller_from_pid(gdbpid)
         if controller:
             orphaned_client_ids = self.remove_gdb_controller(controller)
         else:
-            self.dbprint('could not find gdb controller with pid ' + str(gdbpid))
+            logger.info("could not find gdb controller with pid " + str(gdbpid))
             orphaned_client_ids = []
         return orphaned_client_ids
 
@@ -87,7 +98,7 @@ class StateManager(object):
         try:
             controller.exit()
         except Exception:
-            self.dbprint(traceback.format_exc())
+            logger.error(traceback.format_exc())
         orphaned_client_ids = self.controller_to_client_ids.pop(controller, [])
         return orphaned_client_ids
 
@@ -102,6 +113,7 @@ class StateManager(object):
     def get_pid_from_controller(self, controller):
         if controller and controller.gdb_process:
             return controller.gdb_process.pid
+
         return None
 
     def get_controller_from_pid(self, pid):
@@ -109,16 +121,18 @@ class StateManager(object):
             this_pid = self.get_pid_from_controller(controller)
             if this_pid == pid:
                 return controller
+
         return None
 
     def get_controller_from_client_id(self, client_id):
         for controller, client_ids in self.controller_to_client_ids.items():
             if client_id in client_ids:
                 return controller
+
         return None
 
     def exit_all_gdb_processes(self):
-        self.dbprint('exiting all subprocesses')
+        logger.info("exiting all subprocesses")
         for controller in self.controller_to_client_ids:
             controller.exit()
             self.controller_to_client_ids.pop(controller)
@@ -129,12 +143,12 @@ class StateManager(object):
             if controller.gdb_process:
                 pid = str(controller.gdb_process.pid)
             else:
-                pid = 'process no longer exists'
+                pid = "process no longer exists"
             data[pid] = {
-                    'cmd': ' '.join(controller.cmd),
-                    'abs_gdb_path': controller.abs_gdb_path,
-                    'number_of_connected_browser_tabs': len(client_ids),
-                    'client_ids': client_ids
+                "cmd": " ".join(controller.cmd),
+                "abs_gdb_path": controller.abs_gdb_path,
+                "number_of_connected_browser_tabs": len(client_ids),
+                "client_ids": client_ids,
             }
         return data
 
