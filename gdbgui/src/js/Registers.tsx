eb5958948a34b7c3c40bea67e0bf25f24fcf5299
Greg Baker <ggbaker@sfu.ca>
2023-10-17 22:36:11 -0700

    Fix register display and Python 3.11 compatibility (#463)
    * fix register update condition
    
    * update dependencies for Python 3.11 compatibility
    
    * fix tests
    
    ---------
    
    Co-authored-by: Chad Smith <cs01@users.noreply.github.com>

--- gdbgui/src/js/Registers.tsx
+++ gdbgui/src/js/Registers.tsx
@@ -33,9 +33,8 @@ class Registers extends React.Component<{}, State> {
     let cmds: any = [];
     if (
       [constants.inferior_states.paused, constants.inferior_states.running].indexOf(
-        // @ts-expect-error ts-migrate(2345) FIXME: Argument of type 'boolean' is not assignable to pa... Remove this comment to see the full error message
-        store.get("inferior_program") > -1
-      )
+        store.get("inferior_program")
+      ) == -1
     ) {
       return cmds;
     }
